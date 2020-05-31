c09be9521bc40a1c9ce02b747d752057f97ccb25
Andrey <justscoundrel@yandex.ru>
2020-05-31 22:58:10 +0300

    Add new logic in function

--- app.py
+++ app.py
@@ -27,47 +27,50 @@ def calc():
 @app.route('/success/')
 def result():
 	try:
-		if request.args.get('forme'):
-			message = 'С учетом твоих ежедневных трат'
-			forme = int(request.args.get('forme')) * 12
-			username = request.args.get('name')
-			calc_object = request.args.get('calc_object')
-			price = request.args.get('price')
-			year = int(price) + int(forme)
-			month = year / 12
-			day = month / 30
-			hour = day / 24
-			context = {
-				'name': username,
-				'calc_object': calc_object,
-				'price': price,
-				'year': year,
-				'month': month,
-				'day': day,
-				'hour': hour,
-				'forme':forme,
-				'message':message
-			}
-		else:
-			username = request.args.get('name')
-			calc_object = request.args.get('calc_object')
-			price = request.args.get('price')
-			year = price
-			month = int(price) / 12
-			day = month / 30
-			hour = day / 24
-			context = {
-				'name': username,
-				'calc_object': calc_object,
-				'price': price,
-				'year': year,
-				'month': month,
-				'day': day,
-				'hour': hour
-			}
+		message = 'Твой IQ выше среднего'
+		username = request.args.get('name')
+		calc_object = request.args.get('calc_object')
+		price = request.args.get('price')
+
+
+		monthly_income = request.args.get('monthly_income')
+		year_income = int(monthly_income) * 12
+		day_income = int(monthly_income) / 30
+		hour_income = int(day_income)/ 24
+
+		period = request.args.get('period')
+		period_income = int(year_income) * int(period)
+		price_income = int(price) - int(year_income)
+		month_lack_of = price_income / 12
+		day_lack_of = month_lack_of / 30
+		hour_lack_of = day_lack_of / 24
+		year = price
+		month = int(year) / 12
+		day = month / 30
+		hour = day / 24
+		context = {
+			'name': username,
+			'calc_object': calc_object,
+			'price': price,
+			'year': year,
+			'month': month,
+			'day': day,
+			'hour': hour,
+			'year_income':year_income,
+			'monthly_income':monthly_income,
+			'day_income': day_income,
+			'hour_income': hour_income,
+			'period_income':period_income,
+			'price_income':price_income,
+			'month_lack_of':month_lack_of,
+			'day_lack_of': day_lack_of,
+			'hour_lack_of':hour_lack_of,
+			'message':message
+		}
 		return render_template('result.html', title='Результат', context=context)
-	except:
-		return redirect(url_for('calc'))
+	except IndentationError:
+		message = 'Ваш IQ не соответствует данному сервису.'
+		return redirect(url_for('calc', message=message))
 
 
 
--- form.py
+++ form.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length
+from wtforms.widgets.html5 import NumberInput
 
 class CalculatorForm(FlaskForm):
 	name = StringField ('Ваше Имя', [DataRequired()])
-	calc_object = StringField('Что купить', [DataRequired()])
-	price = IntegerField('Цена', [DataRequired()])
-	forme = IntegerField('Ежемесячные расходы')
+	calc_object = StringField('Что хотите купить', [DataRequired()])
+	price = IntegerField('Цена(руб.)', [DataRequired()], widget=NumberInput())
+	monthly_income = IntegerField('Ежемесячный доход(руб.)', [DataRequired()], widget=NumberInput())
+	period = IntegerField('Период накопления(год)', [DataRequired()], widget=NumberInput())
